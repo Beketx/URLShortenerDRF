cdd196c9f75787f81d6f98dc534ced94f3789872
beketx <beketsk@gmail.com>
2020-05-05 16:55:46 +0600

    change

--- ShortMyUrl/main/models.py
+++ ShortMyUrl/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Model_Short(models.Model):
-    short_url = models.CharField(max_length=50) #for storing
     long_url = models.URLField("URL", unique=True) #for my link
+    short_url = models.CharField(max_length=50) #for storing
     def __str__(self):
         return self.long_url
--- ShortMyUrl/main/views.py
+++ ShortMyUrl/main/views.py
@@ -11,12 +11,12 @@ class LinkAPIView(APIView):
     token = ShortenerOfUrl().create_token()  # save new token by our shortener.py->create_token
 
     def get(self,request,token):
-        # url = get_object_or_404(Model_Short,short_url=token) #[0] the actual object
-        url = Model_Short.objects.filter(short_url=token).first()
+        url = get_object_or_404(Model_Short,short_url=token) #[0] the actual object
+        # url = Model_Short.objects.filter(short_url=token).first()
         serializer = BindSerializer(url)
         # return Response(serializer.data) #redirect to actual url
         # return redirect(serializer.data)
-        return redirect(url.long_url['long_url'])
+        return redirect(request.query_params['long_url'])
         # return HttpResponseRedirect(serializer.data)
 
 
@@ -24,8 +24,8 @@ class BindAPIView(APIView):
     token = ShortenerOfUrl().create_token()  # save new token by our shortener.py->create_token
     def post(self,request):
         form = request.data
-        # new_url = Model_Short.objects.create(long_url=form,short_url=self.token)
-        serializer = BindSerializer(data=form)
+        new_url = Model_Short.objects.create(long_url=form,short_url=self.token)
+        serializer = BindSerializer(data=new_url)
         if serializer.is_valid():
             saved_serializer = serializer.save()
-        return Response(self.token)
+        return Response(serializer.data)
