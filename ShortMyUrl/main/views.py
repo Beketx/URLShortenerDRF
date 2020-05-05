e8bcee1ffe8a500699e0fdfba9e00d0e3955cd53
beketx <beketsk@gmail.com>
2020-05-05 21:10:35 +0600

    2 problems

--- ShortMyUrl/main/views.py
+++ ShortMyUrl/main/views.py
@@ -8,15 +8,15 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 
 class LinkAPIView(APIView):
-    token = ShortenerOfUrl().create_token()  # save new token by our shortener.py->create_token
+    # token = ShortenerOfUrl().create_token()  # save new token by our shortener.py->create_token
 
     def get(self,request,token):
-        url = get_object_or_404(Model_Short,short_url=token) #[0] the actual object
-        # url = Model_Short.objects.filter(short_url=token).first()
+        # url = get_object_or_404(Model_Short,short_url=token) #[0] the actual object
+        url = Model_Short.objects.filter(short_url=token).first()
         serializer = BindSerializer(url)
         # return Response(serializer.data) #redirect to actual url
         # return redirect(serializer.data)
-        return redirect(request.query_params['long_url'])
+        return redirect(url.long_url)
         # return HttpResponseRedirect(serializer.data)
 
 
