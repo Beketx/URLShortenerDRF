24760c41bb7b1e633129a9bba3e94f7e655fbdb0
beketx <beketsk@gmail.com>
2020-05-07 01:38:02 +0600

    Done

--- ShortMyUrl/main/serializers.py
+++ ShortMyUrl/main/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from main.models import Model_Short
 from main.shortener import ShortenerOfUrl
+import uuid
 # class BindSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Model_Short
 #         fields = ['long_url','short_url']
 #
+
 class BindSerializer(serializers.Serializer):
     long_url = serializers.URLField()
     short_url = serializers.UUIDField(default=None)
     token = ShortenerOfUrl().create_token()
     def create(self, validated_data):
-        new_url = Model_Short.objects.create(long_url = validated_data['long_url'],short_url = self.token)
+        new_url = Model_Short.objects.create(long_url = validated_data['long_url'],short_url=self.token)
         return new_url
--- ShortMyUrl/main/shortener.py
+++ ShortMyUrl/main/shortener.py
@@ -8,7 +8,11 @@ class ShortenerOfUrl:
     #     self.token_size = token_size
     def create_token(self):  #rendering random 5 digit token here
         # s = uuid.uuid4()
-        letters = string.ascii_letters
+        letters = random.choice(string.ascii_letters)
         # x = ''.join(random.choice(letters) for i in range(5))
-        return ''.join(hashlib.sha256(letters.encode()).hexdigest())
+        x = hashlib.sha256(letters.encode()).hexdigest()
+        e = x[:8]+'-'+x[8:12]+'-'+x[12:16]+'-'+x[16:20]+'-'+x[20:32]
+        return e
+
+        # letters = random.choice(string.ascii_letters)
         # return ''.join(s)
